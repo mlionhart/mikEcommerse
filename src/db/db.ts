a8371c1c02db478925f410ce4b6884739a57fb30
mlionhart <lionrhart@gmail.com>
2024-05-27 13:30:27 -0600

    fixed deployment issues

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,3 +1,19 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// const db = globalThis.prisma ?? prismaClientSingleton();
+
+// export default db;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
@@ -5,11 +21,15 @@ const prismaClientSingleton = () => {
 };
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
-const db = globalThis.prisma ?? prismaClientSingleton();
+const db = globalThis.prisma || prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
 
 export default db;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,17 +1,36 @@
-// dealing with data caching and everything else built-in to NextJS
+// // dealing with data caching and everything else built-in to NextJS
+// import { unstable_cache as nextCache } from "next/cache";
+// // for request memoization
+// import { cache as reactCache } from "react";
+
+// // basically a function that any number of arguments of any type that returns a Promise of any
+// type Callback = (...args: any[]) => Promise<any>;
+
+// // this function is essentially emulating the NextJS cache built-in cache function
+// export function cache<T extends Callback>(
+//   cb: T,
+//   keyParts: string[],
+//   options: { revalidate?: number | false; tags?: string[] } = {}
+// ) {
+//   // first cache this using React, then nextCashe with keyParts and options
+//   return nextCache(reactCache(cb), keyParts, options)
+// }
+
 import { unstable_cache as nextCache } from "next/cache";
-// for request memoization
 import { cache as reactCache } from "react";
 
-// basically a function that any number of arguments of any type that returns a Promise of any
-type Callback = (...args: any[]) => Promise<any>;
+// Define a callback type that accepts an arbitrary number of arguments and returns a Promise of type T
+type Callback<T> = (...args: unknown[]) => Promise<T>;
 
-// this function is essentially emulating the NextJS cache built-in cache function
-export function cache<T extends Callback>(
-  cb: T,
+// The cache function will take a callback of type T, keyParts, and options, and return a function that returns a Promise of type T
+export function cache<T>(
+  cb: Callback<T>,
   keyParts: string[],
   options: { revalidate?: number | false; tags?: string[] } = {}
-) {
-  // first cache this using React, then nextCashe with keyParts and options
-  return nextCache(reactCache(cb), keyParts, options)
+): Callback<T> {
+  // Return a function that caches using reactCache first and then nextCache with keyParts and options
+  return nextCache(reactCache(cb), keyParts, options) as Callback<T>;
 }
+
+
+
