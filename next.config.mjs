9dc18be79b9db86337da465845a40f421e1409ed
mlionhart <lionrhart@gmail.com>
2024-05-27 20:39:48 -0600

    more image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["econ-site-data.s3.us-east-2.amazonaws.com"], // Add your S3 bucket domain here
+  },
+};
 
 export default nextConfig;
